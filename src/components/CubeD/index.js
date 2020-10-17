94a522efb1ac7015a0864ae74905e5b296d2ca63
senorbeast <hrishikesh99@hotmail.com>
2020-10-18 02:26:47 +0530

    Trying States

--- src/components/CubeD/index.js
+++ src/components/CubeD/index.js
@@ -10,18 +10,8 @@ import {
 //import { CubeD } from './VirtualCube';
 
 function CubePage() {
-    const [newScra, setnewScra] = useState();
-
-    function handleChange(event) {
-        setnewScra(event.target.value);
-        console.log(newScra);
-
-        window.history.pushState(
-            "object or string",
-            "",
-            "/?scramble=" + newScra
-        );
-    }
+    const [newScra, setnewScra] = useState("");
+    const [newSol, setnewSol] = useState("");
 
     return (
         <>
@@ -33,15 +23,40 @@ function CubePage() {
                 </CubeContainer>
                 <ScrambleI>
                     <h1>Scramble </h1>
-                    <InTextArea1 type="Text" onKeyDown={handleChange} />
+                    <InTextArea1
+                        type="Text"
+                        onKeyDown={handleChangeScra}
+                        onKeyUp={handleChangeScra}
+                        placeholder="Enter the Scramble Here :)"
+                    />
                 </ScrambleI>
                 <SolutionI>
                     <h1>Solutions </h1>
-                    <InTextArea2 type="Text" onKeyDown={handleChange} />
+                    <InTextArea2
+                        type="Text"
+                        onClick={handleChangeSol}
+                        onInput={handleChangeSol}
+                        placeholder="Enter the Solution Here :)"
+                    />
                 </SolutionI>
             </CardContainer>
         </>
     );
+
+    function handleChangeScra(event) {
+        setnewScra(event.target.value);
+        console.log("Scramble:", newScra);
+        window.history.pushState(
+            "object or string",
+            "",
+            "/?scramble=" + newScra
+        );
+    }
+
+    function handleChangeSol(event) {
+        setnewSol(event.target.value);
+        console.log("Solution:", newSol);
+    }
 }
 
 export default CubePage;
