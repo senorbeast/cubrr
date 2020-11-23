5faff5b49c618bcaf257e3c463324db3f31e38fe
adityaganesh <39045911+adityaganesh@users.noreply.github.com>
2020-11-23 18:46:10 +0530

    Update cubelets.js
    Changes : - Core added

--- src/components/CubeD/cubelets.js
+++ src/components/CubeD/cubelets.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+
+
 function cubelets_form (scene, size , padding , texture)
 {
     
@@ -14,28 +16,38 @@ function cubelets_form (scene, size , padding , texture)
       for (var k=0; k<3 ;k++)
       {
           //mesh created for every cube
-        // if ( i == 2 && j == 1 && k == 1 )
+        if ( i == 2 && j == 1 && k == 1 )
         
-        // {
-        //     const materials = 
-        //     [
+        {
+            const materials = 
+            [
                 
-        //         new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true ,opacity:1}),
-        //         new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true ,opacity:1}),
-        //         new THREE.MeshBasicMaterial({map:texture}),
-        //         new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true,opacity:1}),
-        //         new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors,transaprent:true ,opacity:1}),
+                new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true ,opacity:1}),
+                new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true ,opacity:1}),
+                new THREE.MeshBasicMaterial({map:texture,transparent1:true,opacity:1}),
+                new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors ,transaprent:true,opacity:1}),
+                new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors,transaprent:true ,opacity:1}),
                
-        //         new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors,transaprent:true ,opacity:1}),
+                new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false, vertexColors: THREE.FaceColors,transaprent:true ,opacity:1}),
                   
-        //     ];
+            ];
               
-        //     var cube2 =  new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5),materials);
+            var cube2 =  new THREE.Mesh(new THREE.BoxGeometry(50,50,50),materials);
 
-        // }
+        }
+        else if ( i ==1 && j ==1 && k ==1 )
+        {
+            var cube2 =  new THREE.Mesh(new THREE.BoxGeometry(50,50,50),new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false,skinning:true, vertexColors: THREE.FaceColors ,transparent:true,opacity:0,side:THREE.DoubleSide}));
+
+        }
          
+        else
+        {
+            var cube2 =  new THREE.Mesh(new THREE.BoxGeometry(50,50,50),new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false,skinning:true, vertexColors: THREE.FaceColors ,transparent:true,opacity:1,side:THREE.DoubleSide}));
+
+        }
+       
         
-        var cube2 =  new THREE.Mesh(new THREE.BoxGeometry(50,50,50),new THREE.MeshBasicMaterial({color: 0xffffff,wireframe: false,skinning:true, vertexColors: THREE.FaceColors ,transparent:true,opacity:1,side:THREE.DoubleSide}));
         scene.add(cube2);
          
          var space,spacex,spacey;
@@ -53,6 +65,26 @@ function cubelets_form (scene, size , padding , texture)
 	}
 		  spacey = spacey+1;
     }
+    for ( var i = 0 ; i<3 ; i++)
+    {
+        const geometry = new THREE.CylinderGeometry( 5, 5, 40, 32 );
+        const material = new THREE.MeshBasicMaterial( {wireframe:false , color: 0x000080} );
+        const cylinder = new THREE.Mesh( geometry, material );
+        cylinder.position.set(0,0,0)
+        if ( i == 1)
+        {
+            cylinder.rotation.z = Math.PI/2
+        }
+
+        if (i == 2)
+        {
+            cylinder.rotation.x = Math.PI/2
+            cylinder.rotation.x = Math.PI/2
+        }
+        scene.add( cylinder );
+
+    }
+    
     
     return meshs
 
