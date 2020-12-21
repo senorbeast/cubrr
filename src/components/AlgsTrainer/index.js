65a3757973d3be86d6663921af45b3cff2613dbb
Hrishikesh Sawant <44683244+senorbeast@users.noreply.github.com>
2020-12-22 00:53:41 +0530

    Apollo intialized

--- src/components/AlgsTrainer/index.js
+++ src/components/AlgsTrainer/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import Navbar from "../Navbar"
+import Navbar from "../Navbar";
 
 function AlgsTrainer(props) {
     return (
         <div>
-            <Navbar toggle={props.toggle} theme={props.theme} setTheme={props.setTheme} />
+            <Navbar
+                toggle={props.toggle}
+                theme={props.theme}
+                setTheme={props.setTheme}
+            />
             <h1>Algs Trainer</h1>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import ApolloProvider from "./ApolloProvider";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
