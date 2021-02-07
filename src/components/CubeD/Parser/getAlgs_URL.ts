73ea31043a3780c737fe563a2a1db6dfa19d4753
Hrishikesh Sawant <44683244+senorbeast@users.noreply.github.com>
2021-02-07 22:02:00 +0530

    getAlgs_URL ☑

---
+++ src/components/CubeD/Parser/getAlgs_URL.ts
@@ -0,0 +1,8 @@
+const getAlgs_URL = (solution: string) => {
+    var regex = /(\/\/).*?(\.)/g;
+    var cmts = solution.replace(regex, "");
+    var cmts2 = cmts.replace(/(\/\/).+/g, "");
+    //Got only Algs
+    return cmts2;
+};
+export default getAlgs_URL;
