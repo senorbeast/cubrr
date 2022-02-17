1cf718d62c80c5a7bb20b1275ebd8cbb27b620ca
senorbeast <hrishikesh99@hotmail.com>
2022-02-17 17:05:33 +0530

    HomePg simplified

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+//import { makeStyles } from '@material-ui/core/styles';
+//import { Typography } from '@material-ui/core';
 import Navbar from '../Navbar';
-//import { CssBaseline } from "@material-ui/core";
-import rubiks_bg from './rubiks_bg.png';
+import { HeroContainer, HeroBg } from '../HomeElements';
 
-const useStyles = makeStyles(() => ({
-    root: {
-        height: 'calc(100vh - 5rem)',
-        backgroundImage: `url(${rubiks_bg})`,
-        minHeight: 'calc(100vh - 5rem)',
-    },
-}));
+//import { CssBaseline } from "@material-ui/core";
+//import rubiks_bg from './rubiks_bg.png';
+
+//const useStyles = makeStyles(() => ({
+//   root: {
+//       height: 'calc(100vh - 5rem)',
+//       backgroundImage: `url(${rubiks_bg})`,
+//       minHeight: 'calc(100vh - 5rem)',
+//    },
+//));
 interface CmProps {
     toggle: () => void;
     theme: string;
     setTheme: React.Dispatch<any>;
 }
 function Home(props: CmProps) {
-    const classes = useStyles();
+    // const classes = useStyles();
     return (
         <>
             <Navbar toggle={props.toggle} theme={props.theme} setTheme={props.setTheme} />
             {/* <CssBaseline /> */}
-            <div className={classes.root}>
-                <Typography>Its Hoome</Typography>
+            <div>
+                {/* <Typography>Its Hoome</Typography> */}
+                <HeroContainer>
+                    <HeroBg>
+                        {/* <VideoBg autoPlay loop muted src={Video} type='video/mp4'></VideoBg> */}
+                    </HeroBg>
+                </HeroContainer>
             </div>
         </>
     );
