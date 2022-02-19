c97884ab0086b70a46c0ca4a5b3936bfda90c6b4
senorbeast <hrishikesh99@hotmail.com>
2022-02-20 03:11:55 +0530

    Make PWA 0.3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import ApolloProvider from './ApolloProvider';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
